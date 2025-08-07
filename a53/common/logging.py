42ac73b659c74b48d7bff3e06bc241b5c529bf1e
d4rk <d4rk@blackvoltage.org>
2025-08-06 20:23:46 -0700

    Log to stderr instead of stdout.

--- a53/common/logging.py
+++ a53/common/logging.py
@@ -5,7 +5,7 @@ def get_logger(name):
     """Creates and returns a logger with the specified name."""
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.StreamHandler(sys.stderr)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
     handler.setFormatter(formatter)
